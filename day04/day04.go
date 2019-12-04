c4a3728a6fb7f3e44cecc70242b2e7e496d28981
Arnaud JOLLY <contact@arnaud-jolly.net>
2019-12-04 22:44:27 +0100

    refactor(day04): introduce PasswordValidator

--- day04/day04.go
+++ day04/day04.go
@@ -6,28 +6,73 @@ import (
 
 // Run is the entrypoint of day04 exercice
 func Run(start, end int) int {
-	count := 0
+	validator := PasswordValidator{}
+
+	validCount := 0
 	for i := start; i <= end; i++ {
-		ok := CheckPasswordPart1(i)
+		ok := validator.Validate(i)
 		if ok {
-			count = count + 1
+			validCount = validCount + 1
 		}
 	}
-	return count
+	return validCount
 }
 
-// CheckPasswordPart1 returns true if the number is a valid password
-func CheckPasswordPart1(i int) bool {
-	s := strconv.Itoa(i)
-	previousChar := byte('0') - 1
-	var twoConsecutiveFound bool
-	for idx := 0; idx < len(s); idx++ {
-		c := s[idx]
-		if previousChar > c {
+// PasswordValidator the main type
+type PasswordValidator struct {
+	digit   byte
+	count   int
+	history []int
+}
+
+// Validate a password
+func (v *PasswordValidator) Validate(password int) bool {
+	s := strconv.Itoa(password)
+	if len(s) != 6 {
+		return false
+	}
+
+	v.history = make([]int, 0)
+	v.digit = s[0]
+	v.count = 0
+
+	for idx := 0; idx < 6; idx++ {
+		ok := v.handle(s[idx])
+		if !ok {
 			return false
 		}
-		twoConsecutiveFound = twoConsecutiveFound || c == previousChar
-		previousChar = c
 	}
-	return twoConsecutiveFound
+	v.history = append(v.history, v.count)
+
+	return v.checkResult()
+}
+
+func (v *PasswordValidator) checkResult() bool {
+	for _, l := range v.history {
+		if l >= 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func (v *PasswordValidator) handle(c byte) bool {
+	if c < v.digit {
+		// if c is decreasing, stop there: it's not valid
+		return false
+	}
+
+	if c != v.digit {
+		// digit has changed
+		v.changeDigit(c)
+	}
+
+	v.count++
+	return true
+}
+
+func (v *PasswordValidator) changeDigit(c byte) {
+	v.history = append(v.history, v.count)
+	v.digit = c
+	v.count = 0
 }
