098424f04b8a76b2d6e584c757bc23d6d9b6ab9b
Arnaud JOLLY <contact@arnaud-jolly.net>
2019-12-04 02:23:15 +0100

    refactor: handle different result types

--- adventofcode2019.go
+++ adventofcode2019.go
@@ -4,27 +4,37 @@ import (
 	"adventofcode2019/common"
 	"adventofcode2019/day01"
 	"adventofcode2019/day02"
+	"adventofcode2019/day03"
 	"flag"
 	"fmt"
 )
 
 func main() {
 
-	dayptr := flag.Int("day", 2, "run the solution for day XX")
+	// specific for day02
 	objptr := flag.Int("objective", 19690720, "objective to get")
-	fptr := flag.String("file", "input.txt", "file path to read from")
-	flag.Parse()
 
-	var result int
-	var err error
+	// common flags
+	fptr := flag.String("file", "input.txt", "file path to read from")
+	dayptr := flag.Int("day", 3, "run the solution for day XX")
+	flag.Parse()
 
 	switch *dayptr {
 	case 1:
-		result, err = day01.Run(*fptr)
+		result, err := day01.Run(*fptr)
+		common.CheckError(err)
+		shareResult(result)
 	case 2:
-		result, err = day02.Run(*objptr, *fptr)
+		result, err := day02.Run(*objptr, *fptr)
+		common.CheckError(err)
+		shareResult(result)
+	case 3:
+		result, err := day03.Run(*fptr)
+		common.CheckError(err)
+		shareResult(result)
 	}
-	common.CheckError(err)
+}
 
-	fmt.Printf("result: %v\n", result)
+func shareResult(result interface{}) {
+	fmt.Printf("Result: %v\n", result)
 }
